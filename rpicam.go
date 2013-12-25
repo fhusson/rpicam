15c1a41edae19d0559209abd1f3b4e6f22246acf
whaly <whaly@baleinoid.fr>
2013-12-25 01:49:06 +0100

    Initial commit

---
+++ rpicam.go
@@ -0,0 +1,77 @@
+package rpicam
+
+import (
+	"os/exec"
+	"strings"
+)
+
+const (
+	raspistillCommand          = "raspistill"
+	raspistillDefaultArguments = "-hf -vf -o - -t 500 --nopreview"
+)
+
+type Manager struct {
+	latest []byte
+	error  []byte
+	call   chan Request
+}
+
+type Response struct {
+	Err  error
+	Data []byte
+}
+
+type Request struct {
+	answer chan Response
+	args   string
+}
+
+func NewManager() *Manager {
+	pm := Manager{}
+	pm.call = make(chan Request)
+
+	return &pm
+}
+
+func (pm *Manager) NewShot(args string) Response {
+	req := Request{}
+	req.answer = make(chan Response)
+	req.args = args
+
+	// we pass the order
+	pm.call <- req
+
+	// we send back the response
+	return <-req.answer
+}
+
+func (pm *Manager) LatestShot() Response {
+	var r Response
+	if len(pm.latest) > 0 {
+		r = Response{}
+		r.Data = pm.latest
+	} else {
+		r = pm.NewShot("")
+	}
+	return r
+}
+
+func (pm *Manager) Serve() {
+	for {
+		select {
+		case request := <-pm.call:
+			response := Response{}
+
+			if request.args != "latest" {
+				args := strings.Split(raspistillDefaultArguments+" "+request.args, " ")
+				out, err := exec.Command(raspistillCommand, args...).CombinedOutput()
+				response.Err = err
+				response.Data = out
+				pm.latest = out
+			} else {
+				response.Data = pm.latest
+			}
+			request.answer <- response
+		}
+	}
+}
---
+++ webclient/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/whaly/rpicam"
+)
+
+func WebcamHandler(w http.ResponseWriter, r *http.Request, pm *rpicam.Manager) {
+	// maxWidth := 2592
+	// maxHeight := 1944
+	args := ""
+
+	width := r.FormValue("width")
+	height := r.FormValue("height")
+	if width != "" && height != "" {
+		args = "-w " + width + " -h " + height
+	}
+	result := pm.NewShot(args)
+
+	if result.Err != nil {
+		http.Error(w, result.Err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "image")
+	w.Write(result.Data)
+}
+
+func main() {
+	fmt.Printf("RPiCam Webclient\n")
+
+	pm := rpicam.NewManager()
+	go pm.Serve()
+
+	http.HandleFunc("/",
+		func(w http.ResponseWriter, r *http.Request) {
+			WebcamHandler(w, r, pm)
+		})
+	http.ListenAndServe(":9001", nil)
+}
